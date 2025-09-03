6948285d6f8796743bc1a13ced084d479c9d846b
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-09-03 12:09:57 +0200

    Debug logging and Cosmos DB id casing Added logging to debug data extraction issues changed Id to id to follow style requirments for Cosmos DB

--- HttpCloud.cs
+++ HttpCloud.cs
@@ -26,20 +26,28 @@ public class HttpCloud
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req
     )
     {
+        //string body = await new StreamReader(req.Body).ReadToEndAsync();
         string body = await req.ReadAsStringAsync();
+        _logger.LogInformation("Bärjar läsa body");
 
         _logger.LogInformation("C# HTTP trigger function processed a request.");
 
         JsonNode data = JsonNode.Parse(body);
+        _logger.LogInformation("parsa body.");
 
-        string name = data["name"].ToString();
+        string? name = data?["name"]?.ToString();
 
-        string email = data["email"].ToString();
+        _logger.LogInformation("Hämta namn.");
+
+        string? email = data?["email"]?.ToString();
+        _logger.LogInformation("hämta email");
+        _logger.LogInformation($"Name: '{name}', Email: '{email}'");
         //Todo om inget namn eller mail är givet, felmeddelande och hantera i frontend.
 
         var message = $"Welcome to Azure Functions!";
 
         var response = req.CreateResponse(HttpStatusCode.OK);
+        _logger.LogInformation("hämta response");
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
         response.WriteStringAsync(message);
 
@@ -48,7 +56,7 @@ public class HttpCloud
         {
             Document = new Visitor
             {
-                Id = System.Guid.NewGuid().ToString(),
+                id = System.Guid.NewGuid().ToString(),
                 Name = name,
                 Email = email,
                 Message = message,
@@ -72,7 +80,7 @@ public class MultiResponse
 
 public class Visitor
 {
-    public string Id { get; set; }
+    public string id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Message { get; set; }
